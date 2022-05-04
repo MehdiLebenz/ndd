e0e11ef485f937d4ec3ada1806bbac9186ad984a
Waniiss <104902015+Waniiss@users.noreply.github.com>
2022-05-04 12:47:20 +0100

    Update button.jsx

--- src/core/button.jsx
+++ src/core/button.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Button from "@mui/material/Button";
 
 
-const ButtonSend = () => {
+const ButtonSend = (props) => {
     return (
         <Button
         type="submit"
         value="Send"
+        disabled={props.isDisabled}
         variant="contained"
         sx={{ mt: 2, mb: 1, bgcolor: "#1d2748" }}
       >
