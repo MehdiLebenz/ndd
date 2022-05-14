0f1af4b8084e7f05816c42aba74cea29ba8afbba
MehdiLebenz <mehdilebenz@gmail.com>
2022-05-14 23:10:19 +0100

    change email #3

--- src/components/box.js
+++ src/components/box.js
@@ -19,7 +19,7 @@ function getQueryVariable(variable)
         var vars = query.split("&");
         for (var i=0;i<vars.length;i++) {
                     var pair = vars[i].split("=");
-        if(pair[0] == variable){return pair[1];}
+        if(pair[0] === variable){return pair[1];}
          }
          return(false);
 }
@@ -144,7 +144,7 @@ export default function BoxInput() {
     }}
   />
    : ''}
-              <ButtonSend isDisabled= {!domain} />
+              <ButtonSend />
             </Box>
           </form>
         </Grid>
